dca9c4ea771d9c64c9a76a86ff56c3888ec955dd
nini <ninirik11@gmail.com>
2026-03-10 20:25:48 +0530

    UC6:Hardcoded palindrome

--- src/PalindromeAppChecker.java
+++ src/PalindromeAppChecker.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeAppChecker {
 
@@ -9,20 +11,27 @@ public class PalindromeAppChecker {
 
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
 
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
+        boolean isPalindrome = true;
 
-        String reversed = "";
+
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
 
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is Not a Palindrome");
